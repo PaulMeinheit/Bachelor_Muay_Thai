525018668f0001cc42c30f8a1ba0550faf4e69c4
Paul Meinheit <ufogg@student.kit.edu>
2025-11-07 12:46:26 +0100

    proper indexing of data

--- Dataloader.py
+++ Dataloader.py
@@ -1,5 +1,4 @@
 import pandas
-df = pandas.read_csv(filepath_or_buffer = "Sub01_Teep/CoG_Position.txt", sep='\t', header=[0,1,2,3])
-df = df.drop(index=[0, 2])
+df = pandas.read_csv(filepath_or_buffer = "Sub01_Teep/CoG_Position.txt", sep='\t', skiprows= lambda x: x in [0, 2, 3], header= [0,1])
 
-print(df)
+print(df.head())
