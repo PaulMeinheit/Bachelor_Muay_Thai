0c116cb3e2da297f8fb7b76e4d505049992e3257
Paul Meinheit <ufogg@student.kit.edu>
2025-11-17 16:03:54 +0100

    added 3d plot

--- Dataloader.py
+++ Dataloader.py
@@ -1,4 +1,18 @@
 import pandas
-df = pandas.read_csv(filepath_or_buffer = "Sub01_Teep/CoG_Position.txt", sep='\t', skiprows= lambda x: x in [0, 2, 3], header= [0,1])
+import matplotlib
+import matplotlib.pyplot as plt
+loadedData = pandas.read_csv(filepath_or_buffer = "Sub01_Teep/JointPositions.txt", sep='\t', skiprows= lambda x: x in [0, 2, 3], header= [0,1])
 
-print(df.head())
+JointToPlot = loadedData["R_ANKLE_POSITION"]
+
+
+ax = plt.figure().add_subplot(projection='3d')
+
+
+ax.plot(JointToPlot['X'], JointToPlot['Y'], JointToPlot['Z'])
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+plt.show()
