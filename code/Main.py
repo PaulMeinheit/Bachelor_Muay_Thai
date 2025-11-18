a0f8b02d38d5b1b6d18312e5df9cdde233a43809
Paul Meinheit <ufogg@student.kit.edu>
2025-11-18 18:00:40 +0100

    added impact frame detection

--- code/Main.py
+++ code/Main.py
@@ -1,12 +1,16 @@
 import Slicer
 import os
+import SegmentFinder
 
 dirPath = "Sub01_Teep"
-resultPath = "results"
-segmentframes = [0,100,200,300,400,500,600,700,800,900,1000]
+intermediaryResultPath = "results"
+segmentBeginFrames = [0,100,200,300,400,500,600,700,800,900,1000]
+segmentImpactFrames = [50,150,250,350,450,550,650,750,850,950,1050]
 
-
-os.mkdir(resultPath)
 for file in os.listdir(dirPath):
-    Slicer.sliceData(os.path.join(dirPath,file),resultPath,segmentframes)
+    Slicer.sliceData(os.path.join(dirPath,file),intermediaryResultPath,segmentBeginFrames)
+
+grfPath = "results/" + dirPath + "/GRF_filtered12hz_resampled120hz.txt"
+SegmentFinder.findSegments(grfPath, segmentImpactFrames)
+
 
--- code/SegmentFinder.py
+++ code/SegmentFinder.py
@@ -2,6 +2,21 @@
 ##Recall the five phases and points of segmentation:
 ##
 ##|       Preparation             |          Extension              |                Retraction                    |            Termination               |
-##0 [stand still] ------> 1 [lift kick foot] 0 ------> 1 [strike / max knee extension] 0 ----------------> 1 [kick foot on gnd] 0 -------------> [Kick ends]
-def findSegments(groundReactionData):
-    return
+##0 [stand still] ------> 1 [lift kick foot] 0 ------> 1 [strike / max knee extension] 0 ----------------> 1 [kick foot on gnd] 0 -------------> [Kick ends]ta)
+import pandas
+import os
+def findTeepSegments(groundReactionPath,impactFrames):
+    segmentData = [[]]
+    counter = 0
+    for file in os.listdir(groundReactionPath):
+        data = pandas.read_csv(os.path.join(groundReactionPath,file))
+    #lift off frame
+    segmentData[counter][1] = impactFrames[counter]
+    #TODO
+    #impact frame
+    segmentData[counter][1] = impactFrames[counter]
+    #foot down frame
+    #TODO
+    segmentData[counter][1] = impactFrames[counter]
+    counter =+ 1
+    return segmentData
