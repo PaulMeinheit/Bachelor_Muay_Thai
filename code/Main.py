7d6a50a3a035671a15a54cda110aa2432571d71b
Paul Meinheit <ufogg@student.kit.edu>
2025-11-18 16:01:14 +0100

    added segmentfinder

--- code/Main.py
+++ code/Main.py
@@ -1,7 +1,7 @@
-import Segmenter
+import code.Slicer as Slicer
 filePath = "Sub01_Teep/JointAngles.txt"
 resultPath = "results"
 segmentframes = [0,100,200,300,400,500,600,700,800,900]
 trialName = "Sub01_Teep_finishedJointAngles"
-Segmenter.segmentData(filePath,resultPath,segmentframes,trialName)
+Slicer.segmentData(filePath,resultPath,segmentframes,trialName)
 
---
+++ code/SegmentFinder.py
@@ -0,0 +1,6 @@
+
+##Recall the five phases and points of segmentation:
+##
+##|       Preparation             |          Extension              |                Retraction                    |            Termination               |
+##0 [stand still] ------> 1 [lift kick foot] 0 ------> 1 [strike / max knee extension] 0 ----------------> 1 [kick foot on gnd] 0 -------------> [Kick ends]
+def findSegments()
