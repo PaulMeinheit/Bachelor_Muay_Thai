0d266bc67ba197e5d31df9ae7816f18eff3338ee
Paul Meinheit <ufogg@student.kit.edu>
2025-11-24 11:00:56 +0100

    changed plots

--- code/Ploter.py
+++ code/Ploter.py
@@ -3,7 +3,7 @@ import Dataloader
 import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  # needed for 3D plots
-
+import os
 
 data = Dataloader.loadDataNoSkip("AveragedResults/CoG_Position.txt.csv")
 dataToPlot = data["FullBody_CoG_pos"]
@@ -21,5 +21,49 @@ ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 
-# Show plot
+os.makedirs("plots", exist_ok=True)                    # create folder if missing
+out_path = os.path.join("plots", "CoG_plot.png")       # file name + folder
+fig.savefig(out_path, dpi=300, bbox_inches='tight')    # save high-res PNG
+plt.show()
+
+
+# load averaged data
+df = Dataloader.loadDataNoSkip("AveragedResults/GRF_filtered12hz_resampled120hz.txt.csv")
+
+# If your DataFrame has MultiIndex columns (level0=sensor, level1=axis) access with a tuple:
+z = df[("FP2", "Z")]
+
+# Simple plot
+plt.figure(figsize=(10, 4))
+plt.plot(z.index, z.values, label="FP2 Z")
+plt.xlabel("Frame")
+plt.ylabel("Z (force)")
+plt.title("FP2 Z time-series")
+plt.grid(True)
+plt.legend()
+plt.show()
+# Save plot
+plt.savefig(os.path.join("plots", "FP2_Z_plot.png"), dpi=300, bbox_inches='tight')
+
+
+data = Dataloader.loadDataNoSkip("AveragedResults/JointPositions.txt.csv")
+dataToPlot = data["L_ANKLE_POSITION"]
+
+# Create a 3D figure
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot points
+ax.scatter(dataToPlot['X'], dataToPlot['Y'], dataToPlot['Z'], c='blue', marker='o')
+ax.margins(x=1,z=2)
+
+# Label axes
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+#save plot
+os.makedirs("plots", exist_ok=True)                    # create folder if missing
+out_path = os.path.join("plots", "L_ANKLE_POSITION_plot.png")
+fig.savefig(out_path, dpi=300, bbox_inches='tight')    # save high-res PNG
 plt.show()
